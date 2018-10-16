84a0beccfb2e6a5691fec67944b829fc70085c26
estimon <ploompuumartin@gmail.com>
2018-10-16 11:34:55 +0300

    16.10

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parties = new List<Party>();
+            parties.Add(new Company { Id = 1, Name = "Delfi", RegNo = "12313" });
+            parties.Add(new Person { Id = 2, Firstname = "Siim", Lastname = "Tiilen", RegNo = "32978129821879132789387912789132" });
+            parties.Add(new Person { Id = 3, Firstname = "Siim", Lastname = "Tiilen", RegNo = "32978129821879132789387912789131" });
+
+
+
+            foreach (var party in parties)
+            {
+                Console.WriteLine(party.DisplayName + " (" + party.RegNo + ")");
+            }
+
+            Console.WriteLine("Press any key to continue....");
+            Console.ReadKey();
+        }
+
+        public abstract class Party
+        {
+            public int Id { get; set; }
+            public string RegNo { get; set; }
+            public abstract string DisplayName { get;}
+        }
+
+        public class Person : Party
+        {
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+
+            public override string DisplayName { get { return Lastname + " , " + Firstname; } }
+
+
+        }
+
+        public class Company : Party
+        {
+            public string Name { get; set; }
+
+            public override string DisplayName { get { return Name; } }
+        }
+
+    }
+}
---
+++ itbelikethat/itbelikethat/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace itbelikethat
+{
+    class Program
+    {
+        static FallbackLogger _logger = new FallbackLogger();
+
+        static void Main(string[] args)
+        {
+            _logger.AddLogger(new SeriLogLoggerWrapper());
+            _logger.AddLogger(new FileLoggerWrapper());
+
+            _logger.Info("Program started");
+
+            Console.WriteLine("Press any key to continue ....");
+            Console.ReadKey();
+        }
+
+        public interface ILogger
+        {
+            void Info(string message);
+            void Error(string message);
+        }
+
+        public class FallbackLogger : ILogger
+        {
+            private IList<ILogger> _logger = new List<ILogger>();
+
+            public void AddLogger(ILogger logger)
+            {
+                _logger.Add(logger);
+            }
+
+            public void Error(string message)
+            {
+                foreach(var logger in _logger)
+                {
+                    try
+                    {
+                        logger.Error(message);
+                        return;
+
+                    }
+                    catch { }
+                }
+            }
+
+            public void Info(string message)
+            {
+                foreach (var logger in _logger)
+                {
+                    try
+                    {
+                        logger.Info(message);
+                        return;
+
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(logger.GetType() + ex.Message);
+                    }
+                    
+                }
+            }
+        }
+
+        public class FileLoggerWrapper : ILogger
+        {
+            private FileLogger  _fileLogger = new FileLogger();
+
+            public void Error(string message)
+            {
+                _fileLogger.WriteError(message);
+            }
+
+            public void Info(string message)
+            {
+                _fileLogger.WriteInfo(message);
+            }
+        }
+
+        public class SeriLogLoggerWrapper : ILogger
+        {
+            private SerilogLogger _serilogLogger = new SerilogLogger();
+
+            public void Error(string message)
+            {
+                _serilogLogger.LogError(message);
+            }
+
+            public void Info(string message)
+            {
+                _serilogLogger.LogInfo(message);
+            }
+        }
+
+        public class FileLogger
+        {
+            public void WriteInfo(string message)
+            {
+                Console.WriteLine("Filelogger: info " + message);
+            }
+
+            public void WriteError(string message)
+            {
+                Console.WriteLine("Filelogger: Error " + message);
+            }   
+        }
+        
+        public class SerilogLogger
+        {
+            public void LogInfo(string message)
+            {
+                throw new Exception();
+                Console.WriteLine("Serilogger: info " + message);
+            }
+
+            public void LogError(string message)
+            {
+                throw new Exception();
+            
+                Console.WriteLine("Serilogger: Error " + message);
+            }
+        }
+    }
+}
